60b10e30042eca386e88f3648983ef80e2d9966e
vishwam7 <shahvishwam7@gmail.com>
2024-04-08 18:25:36 -0400

    up until now

--- src/spiders/stocks_spider.py
+++ src/spiders/stocks_spider.py
@@ -8,92 +8,105 @@ from scrapy.shell import inspect_response
 class StocksSpider(scrapy.Spider):
   name = 'stocks'
   start_urls = [
-    'https://finance.yahoo.com/screener/predefined/ms_basic_materials?offset=0&count=100',
-    'https://finance.yahoo.com/screener/predefined/ms_communication_services?offset=0&count=100',
-    'https://finance.yahoo.com/screener/predefined/ms_consumer_cyclical?offset=0&count=100',
-    'https://finance.yahoo.com/screener/predefined/ms_consumer_defensive?offset=0&count=100',
-    'https://finance.yahoo.com/screener/predefined/ms_energy?offset=0&count=100',
-    'https://finance.yahoo.com/screener/predefined/ms_financial_services?offset=0&count=100',
-    'https://finance.yahoo.com/screener/predefined/ms_healthcare?offset=0&count=100',
-    'https://finance.yahoo.com/screener/predefined/ms_industrials?offset=0&count=100',
-    'https://finance.yahoo.com/screener/predefined/ms_real_estate?offset=0&count=100',
-    'https://finance.yahoo.com/screener/predefined/ms_technology?offset=0&count=100',
-    'https://finance.yahoo.com/screener/predefined/ms_utilities?offset=0&count=100',
+    'https://finance.yahoo.com/sectors/basic-materials/?offset=0&count=100',
+    'https://finance.yahoo.com/sectors/communication-services/?offset=0&count=100',
+    'https://finance.yahoo.com/sectors/consumer-cyclical/?offset=0&count=100',
+    'https://finance.yahoo.com/sectors/consumer-defensive/?offset=0&count=100',
+    'https://finance.yahoo.com/sectors/energy/?offset=0&count=100',
+    'https://finance.yahoo.com/sectors/financial-services/?offset=0&count=100',
+    'https://finance.yahoo.com/sectors/healthcare/?offset=0&count=100',
+    'https://finance.yahoo.com/sectors/industrials/?offset=0&count=100',
+    'https://finance.yahoo.com/sectors/real-estate/?offset=0&count=100',
+    'https://finance.yahoo.com/sectors/technology/?offset=0&count=100',
+    'https://finance.yahoo.com/sectors/utilities/?offset=0&count=100',
   ]
 
+  def convert_to_number(self, value):
+    multiplier = 1
+    if 'B' in value:
+        multiplier = 1e9
+    elif 'M' in value:
+        multiplier = 1e6
+    elif 'K' in value:
+        multiplier = 1e3
+    elif 'T' in value:
+        multiplier = 1e12
+
+    # Remove any non-numeric characters except '.'
+    numeric_value = ''.join(filter(lambda x: x.isdigit() or x == '.', value))
+    return float(numeric_value) * multiplier if numeric_value else None
+  
+  def extract_sector_name(self, url):
+    # Extract the sector name from the URL using regular expression
+    match = re.search(r'/sectors/([^/?]*)', url)
+    if match:
+        # Replace hyphens with spaces and capitalize each word
+        return match.group(1).replace('-', ' ').title()
+    return None
+  
   def parse(self, response):
-    sector = re.search("ms_[^?]+", response.url).group()[3:]
-    stocks = []
-    amt = { 'k': 1000, 'M': 1000000, 'B': 1000000000, 'T': 1000000000000 }
-    offset = int(re.search("offset=\d+", response.url).group()[7:])
+    sector_name = self.extract_sector_name(response.url)
+    for row in response.xpath('//table[contains(@class, "expandable-table")]/tbody/tr'):
+        item = Stock()
+        # Extract the stock symbol and company name from the link's text
+        item['symbol'] = row.xpath('.//td[1]//span[contains(@class, "symbol")]/text()').get()
+        
+        # Extracting the company name
+        item['company_name'] = row.xpath('.//td[1]//span[contains(@class, "longName")]/text()').get()
 
-    res_count = response.xpath('//div[@id="fin-scr-res-table"]/div[1]/div[1]/span[2]//text()').get()
-    if res_count != None:
-      minimum, maximum, total = int(res_count.split('-')[0]), int(res_count.split('-')[1].split(' ')[0]), int(res_count.split('-')[1].split(' ')[2])
-    else:
-      print('Could not find the results listed, most likely there is no stock information for some reason')
-      # Check if a page refresh is in order, if it is, yield new request, else exit with error code giving debug reason
-      needs_refresh = None #response.xpath('').get()
-      if needs_refresh != None:
-        # Indicates the page needs a refresh, let's initiate the same request.
-        return scrapy.Request(response.url, self.parse)
-      else:
-        # We're not sure what the error is, further debugging may be necessary.
-        # TODO: Log some kind of error message and stop the spider from running
-        print('Some unknown error occurred')
-        return []
+        # Extracting and converting the last_price
+        last_price = row.xpath('.//td[2]/span/text()').get()
+        item['last_price'] = self.convert_to_number(last_price) if last_price else None
+        print("Inserting last_price:", item['last_price'])
 
-    # Populate the number of stocks listed on the current page with the current sector
-    for _ in range(maximum - minimum + 1):
-      stocks.append(Stock(sector=sector))
+        # Extracting and converting the target_price (1Y Target Est.)
+        target_price = row.xpath('.//td[3]/span/text()').get()
+        item['target_price'] = self.convert_to_number(target_price) if target_price else None
+        print("Inserting target_price:", item['target_price'])
 
-    for i, ticker in enumerate(response.xpath('//tbody/tr/td[@aria-label="Symbol"]//text()').extract()):
-      stocks[i]['ticker'] = ticker
 
-    for i, name in enumerate(response.xpath('//tbody/tr/td[@aria-label="Symbol"]/a/@title').extract()):
-      stocks[i]['name'] = name
+        market_weight = row.xpath('.//td[4]/span/text()').get()
+        if market_weight:
+            market_weight = market_weight.replace('%', '').strip()
+            item['market_weight'] = float(market_weight) if market_weight else None
+        print("Inserting market_weight:", item['market_weight'])
+        
+        
+        # Extracting data from <fin-streamer> elements; this might require JavaScript rendering
+        # If the data isn't loaded properly, you might need to use Scrapy-Splash or Selenium
+        market_cap = row.xpath('.//td[5]/span/fin-streamer/@data-value').get()
+        if market_cap:
+           item['market_cap'] = self.convert_to_number(market_cap) if market_cap else None
+        print("Inserting market_cap:", item['market_cap'])  
+        # Extracting the Day Change Percentage
+        # Note: The day change percentage is contained within a fin-streamer element, which might be dynamically loaded.
+        # If the values are not being extracted, it might be due to the page's dynamic content loading.
+  
+        day_change_percentage = row.xpath('.//td[6]//fin-streamer[contains(@data-field, "regMarketChangePercent")]/@data-value').get()
+        if day_change_percentage:
+            item['day_change_percentage'] = day_change_percentage.strip() + '%'  # Adding '%' back if needed
+        else:
+            item['day_change_percentage'] = None
+        # Extracting the YTD Return
+        print("Inserting day_change_percentage:", item['day_change_percentage'])  
+        
+        
+        ytd_return = row.xpath('.//td[7]//fin-streamer[contains(@data-field, "ytdReturn")]/span/text()').get()
+        # The extracted value could include '+' or '-', so it's directly usable as a string.
+        # If you need to convert it to a numerical type, ensure to handle the conversion appropriately.
+        if ytd_return:
+            item['ytd_return'] = ytd_return.strip()
+        else:
+            item['ytd_return'] = None  # or some default value or handling
+        print("Inserting ytd_return:", item['ytd_return']) 
+        
+        
+        # Analyst rating might require some logic if there are different classes for Buy/Hold etc.
+        buy_hold_recommendation = row.xpath('.//td[8]//div[@data-testid="table-cell-rating"]/text()').get()
+        item['buy_hold_recommendation'] = buy_hold_recommendation.strip() if buy_hold_recommendation else None
+        print("Inserting buy_hold_recommendation:", item['buy_hold_recommendation'])
 
-    for i, intraday_price in enumerate(response.xpath('//tbody/tr/td[@aria-label="Price (Intraday)"]//text()').extract()):
-      stocks[i]['intraday_price'] = intraday_price.replace(',','')
+        # These items need to be filled with your own logic or adjusted according to the page structure
+        item['sector_name'] = sector_name  # Example static assignment
 
-    for i, price_change in enumerate(response.xpath('//tbody/tr/td[@aria-label="Change"]//text()').extract()):
-      stocks[i]['price_change'] = price_change.replace(',','')
-
-    for i, percent_change in enumerate(response.xpath('//tbody/tr/td[@aria-label="% Change"]//text()').extract()):
-      stocks[i]['percent_change'] = percent_change[:-1].replace(',','')
-
-    for i, volume in enumerate(response.xpath('//tbody/tr/td[@aria-label="Volume"]//text()').extract()):
-      volume = volume.replace(',','')
-      stocks[i]['volume'] = str(float(volume[:-1]) * amt[volume[-1]]) if volume[-1] in amt.keys() else volume
-
-    for i, avg_vol_3_month in enumerate(response.xpath('//tbody/tr/td[@aria-label="Avg Vol (3 month)"]//text()').extract()):
-      if avg_vol_3_month == 'N/A':
-        stocks[i]['avg_vol_3_month'] = None
-      else:
-        avg_vol_3_month = avg_vol_3_month.replace(',','')
-        stocks[i]['avg_vol_3_month'] = str(float(avg_vol_3_month[:-1]) * amt[avg_vol_3_month[-1]]) if avg_vol_3_month[-1] in amt.keys() else avg_vol_3_month
-
-    for i, market_cap in enumerate(response.xpath('//tbody/tr/td[@aria-label="Market Cap"]//text()').extract()):
-      if market_cap == 'N/A':
-        stocks[i]['market_cap'] = None
-      else:
-        market_cap = market_cap.replace(',','')
-        stocks[i]['market_cap'] = str(float(market_cap[:-1]) * amt[market_cap[-1]]) if market_cap[-1] in amt.keys() else market_cap
-
-    for i, pe_ratio_ttm in enumerate(response.xpath('//tbody/tr/td[@aria-label="PE Ratio (TTM)"]//text()').extract()):
-      if pe_ratio_ttm == 'N/A':
-        stocks[i]['pe_ratio_ttm'] = None
-      else:
-        pe_ratio_ttm = pe_ratio_ttm.replace(',','')
-        stocks[i]['pe_ratio_ttm'] = str(float(pe_ratio_ttm[:-1]) * amt[pe_ratio_ttm[-1]]) if pe_ratio_ttm[-1] in amt.keys() else pe_ratio_ttm
-
-    # Check if we have reached the last stock with the current filters
-    if maximum < total:
-      # If we have not, increase the offset by 100 and yield a new request for the next 100 stocks
-      offset = offset + 100
-      newUrl = re.sub('offset=\d+', 'offset=' + str(offset), response.url)
-      yield scrapy.Request(newUrl, self.parse)
-
-    # Submit the information on the current 100 stocks we are inspecting
-    for stock in stocks:
-      yield stock
+        yield item
